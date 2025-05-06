06c0f464f7be039104e544591202f0963bc7ede8
MuFaqihi <mmnouc2@gmail.com>
2025-05-06 09:37:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('start-game').addEventListener('click', function() {
+    alert('لعبة جديدة تبدأ الآن!');
+});
+
+document.getElementById('join-game').addEventListener('click', function() {
+    alert('انضممت إلى لعبة!');
+});
